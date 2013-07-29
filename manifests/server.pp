37c99db564a703090aad932bacb042fd5b1fb926
Martin Magr <mmagr@redhat.com>
2013-07-29 08:55:45 +0200

    Enable service on boot

--- manifests/server.pp
+++ manifests/server.pp
@@ -9,6 +9,7 @@ class qpid::server(
   $cluster_package_ensure = present,
   $service_name = 'qpidd',
   $service_ensure = running,
+  $service_enable = true,
   $port = '5672',
   $max_connections = '500',
   $worker_threads = '17',
@@ -58,6 +59,7 @@ class qpid::server(
 
   service { $service_name:
     ensure => $service_ensure,
+    enable => $service_enable,
     subscribe => [Package[$package_name], File[$config_file]]
   }
 
